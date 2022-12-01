010c5756946c7677f261284b7029cee4526d9a01
Siebe Bosch <siebe@hydroconsult.nl>
2022-12-01 09:27:26 +0100

    'gitignore'

---
+++ python/read.py
@@ -0,0 +1,7 @@
+
+import geopandas as gpd
+import pandas as pd
+
+gdf_wqpoints = gpd.read_file(rf'c:\GITHUB\klimaatatlas\data\WQPoints.geojson')
+
+gdf_watergang_vlak = gpd.read_file(rf'c:\GITHUB\klimaatatlas\data\WQPoints.geojson')
