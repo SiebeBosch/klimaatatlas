3af27f21d7f17a87774de1760ee03143921359f7
Siebe Bosch <siebe@hydroconsult.nl>
2022-12-01 10:08:31 +0100

    'setting_paths'

--- python/read.py
+++
@@ -1,7 +0,0 @@
-
-import geopandas as gpd
-import pandas as pd
-
-gdf_wqpoints = gpd.read_file(rf'c:\GITHUB\klimaatatlas\data\WQPoints.geojson')
-
-gdf_watergang_vlak = gpd.read_file(rf'c:\GITHUB\klimaatatlas\data\WQPoints.geojson')
