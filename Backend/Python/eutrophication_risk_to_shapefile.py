92d052d87cbbd8b4bb5791232ac2d15fb5e36b82
Siebe Bosch <siebe@hydroconsult.nl>
2023-05-31 12:28:07 +0200

    'implementation_rules_with_rating'

--- Backend/Python/eutrophication_risk_to_shapefile.py
+++ Backend/Python/eutrophication_risk_to_shapefile.py
@@ -18,14 +18,16 @@ def execute_query(query):
     rows = cur.fetchall()
     return {row[0]: row[1] for row in rows}
 
-# Read 95 percentile concentrations of Nitrogen (Nt) and Phosphorus (Pt)
-query_Nt = "SELECT featureidx, datavalue FROM WQPOLYGONSTATISTICS WHERE substance='Nt' AND percentile=95 AND DATASOURCE='SOBEK';"
-query_Pt = "SELECT featureidx, datavalue FROM WQPOLYGONSTATISTICS WHERE substance='Pt' AND percentile=95 AND DATASOURCE='SOBEK';"
-Nt_values = execute_query(query_Nt)
-Pt_values = execute_query(query_Pt)
+# Function to get unique climate scenarios
+def get_scenarios():
+    cur = conn.cursor()
+    cur.execute("SELECT DISTINCT SCENARIO FROM WQPOLYGONSTATISTICS")
+    return [row[0] for row in cur.fetchall()]
 
 # Calculate eutrofication risk indicator value for each feature
 def calculate_eutrofication(row, Nt_values, Pt_values):
+    feature_idx = row.name
+    print(f"Processing feature index: {feature_idx}")  # print statement
     feature_idx = row.name
     waterdiepte = max(row["WATERDIEPT"], 0.5)
     Nt = Nt_values.get(feature_idx, 0)
@@ -35,7 +37,16 @@ def calculate_eutrofication(row, Nt_values, Pt_values):
         eutrophic *= 1.5
     return eutrophic
 
-gdf["eutrophic"] = gdf.apply(calculate_eutrofication, axis=1, args=(Nt_values, Pt_values))
+# Iterate over all unique climate scenarios
+for scenario in get_scenarios():
+    # Read 95 percentile concentrations of Nitrogen (Nt) and Phosphorus (Pt)
+    query_Nt = f"SELECT featureidx, datavalue FROM WQPOLYGONSTATISTICS WHERE substance='Nt' AND percentile=95 AND DATASOURCE='SOBEK' AND SCENARIO='{scenario}';"
+    query_Pt = f"SELECT featureidx, datavalue FROM WQPOLYGONSTATISTICS WHERE substance='Pt' AND percentile=95 AND DATASOURCE='SOBEK' AND SCENARIO='{scenario}';"
+    Nt_values = execute_query(query_Nt)
+    Pt_values = execute_query(query_Pt)
+    
+    # Calculate eutrophication and write to a column named after the scenario
+    gdf[scenario] = gdf.apply(calculate_eutrofication, axis=1, args=(Nt_values, Pt_values))
 
 # Write the result back to the polygon shapefile
 gdf.to_file(shp_path)
